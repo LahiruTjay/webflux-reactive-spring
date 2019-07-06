4bb8a628ddc0c92cfec92f268c4ae3abb8959834
LahiruTjay <lahiru.tjay@gmail.com>
2019-07-06 21:29:19 +0530

    Server Sent Events - multiple ways added.

--- src/main/java/com/example/reactive/controller/ReactiveController.java
+++ src/main/java/com/example/reactive/controller/ReactiveController.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,12 +37,40 @@ public class ReactiveController {
     Flux<WebFluxEvent> getEventStream() {
         Flux<WebFluxEvent> eventFlux = Flux.fromStream(Stream.generate(() -> new WebFluxEvent(new Random().nextLong(), new Date())));
         Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(1));
-        return Flux.zip(eventFlux, durationFlux).map(Tuple2::getT1);
+        return Flux.zip(durationFlux, eventFlux)
+            .map(Tuple2::getT2);
     }
 
     @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamFlux() {
-        return Flux.interval(Duration.ofSeconds(1)).map(sequence -> "Flux - " + LocalTime.now().toString());
+        return Flux.interval(Duration.ofSeconds(1))
+            .map(sequence -> "Flux - " + LocalTime.now()
+                .toString());
+    }
+
+    @GetMapping(value = "/notifyonEvent", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getData() {
+        Random r = new Random();
+        int low = 0;
+        int high = 50;
+        return Flux.fromStream(Stream.generate(() -> r.nextInt(high - low) + low)
+            .map(s -> String.valueOf(s))
+            .peek((msg) -> {
+                System.out.println(msg);
+            }))
+            .map(s -> s)
+            .delayElements(Duration.ofSeconds(1));
+    }
+
+    @GetMapping("/stream-sse")
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+            .map(sequence -> ServerSentEvent.<String> builder()
+                .id(String.valueOf(sequence))
+                .event("periodic-event")
+                .data("SSE - " + LocalTime.now()
+                    .toString())
+                .build());
     }
 
 }
